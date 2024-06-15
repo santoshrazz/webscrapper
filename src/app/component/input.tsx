0a8978b8c0910be0d072ed63a29d8b85ecd3951e
santoshrazz <santoshrajbgp11@gmail.com>
2024-06-15 09:24:08 +0530

    added navbar and home page

---
+++ src/app/component/input.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const InputComponent = () => {
+  return (
+    <input
+      className="p-4 m-4 min-w-36 bg-slate-400/30 focus:border-none focus:outline-none text-black"
+      placeholder="Enter Link"
+    ></input>
+  );
+};
